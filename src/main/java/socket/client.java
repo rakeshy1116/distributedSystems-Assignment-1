2b6f80f9ba4656c0753050858feb77b4b6369d51
Rakesh Chowdary Yarlagadda <yrakeshchowdary1116@gmail.com>
2023-02-05 18:59:39 -0700

    initial commit

---
+++ src/main/java/socket/client.java
@@ -0,0 +1,27 @@
+package socket;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class client {
+    public static void main(String[] args) throws IOException {
+        try {
+            Socket clientSocket = new Socket("127.0.0.1", 5555);
+            PrintWriter out = new PrintWriter(clientSocket.getOutputStream());
+            BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+            String message = in.readLine();
+            out.println(message);
+            in.close();
+            out.close();
+            clientSocket.close();
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+
+    }
+}
